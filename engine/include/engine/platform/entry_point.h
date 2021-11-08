79d7f2972dd131366f80eaf44dc1aa2cdf7fa207
Gui Lin <guileen@gmail.com>
2021-11-09 00:00:52 +0800

    add a simple log tool

--- engine/include/engine/platform/entry_point.h
+++ engine/include/engine/platform/entry_point.h
@@ -4,12 +4,13 @@
 #include <engine/engine.h>
 #include <engine/service_locator.h>
 #include <glm/glm.hpp>
-
+#include <engine/log.h>
 #include <engine/platform/game.h>
 
 int main(int argc, char **argv) {
     auto dir = weakly_canonical(std::filesystem::path(argv[0])).parent_path();
     ServiceLocator::SetExePath(dir);
+    info("Current working directory: %s", dir.c_str());
     // create the game
     auto* theGame = CreateGame();
     theGame->Run();
